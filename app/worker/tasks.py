b84ffbff8754b96e601fe3def6c7940ebda2301b
jafow <jared.a.fowler@gmail.com>
2020-02-02 01:07:22 -0800

    some bad formatting

--- app/worker/tasks.py
+++ app/worker/tasks.py
@@ -3,4 +3,8 @@ from app.celery import queue
 
 @queue.task
 def upload_chunk(chunkid, buf):
-    pass
+    a = {
+            "key": "val",
+            "x": [1, 2, 3, 4, 1, 2, 3, 4, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3] 
+            }
+    return a
